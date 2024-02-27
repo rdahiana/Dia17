d89e0487672a70023f42e531df90f628b6da08a6
Ruth Rivas <rivasruthdahiana@gmail.com>
2024-02-27 09:16:43 -0300

    inicializacion de proyecto

---
+++ src/main/java/com/sistema/proyecto/SistemaWifiApplication.java
@@ -0,0 +1,14 @@
+package com.sistema.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaWifiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SistemaWifiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/proyecto/SistemaWifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaWifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
